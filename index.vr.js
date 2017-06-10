47b096900343b64aca2853e3c2aa75c3b07e7bb3
DaniyarJakupov <ddr199305@gmail.com>
2017-06-10 20:28:09 +0200

    Run prettier

--- index.vr.js
+++ index.vr.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import {AppRegistry, asset, Pano, Text, View} from 'react-vr';
+import React from "react";
+import { AppRegistry, asset, Pano, Text, View } from "react-vr";
 
 const places = [
   {
-    name: 'S Bahn',
-    image: 'sbahn.jpg',
-    id: 1,
+    name: "S Bahn",
+    image: "sbahn.jpg",
+    id: 1
   },
   {
-    name: 'S Bahn Entrance',
-    image: 'sbahn-top.jpg',
-    id: 2,
-  },
+    name: "S Bahn Entrance",
+    image: "sbahn-top.jpg",
+    id: 2
+  }
 ];
 
 export default class Campus extends React.Component {
   constructor() {
     super();
-    this.state = {showNav: false, place: 'sbahn.jpg'};
+    this.state = { showNav: false, place: "sbahn.jpg" };
   }
 
   toggleNav() {
-    this.setState({showNav: !this.state.showNav});
+    this.setState({ showNav: !this.state.showNav });
   }
 
   renderText() {
     return (
       <Text style={styles.navText}>
-        {this.state.showNav ? 'Close Navigation' : 'Open Navigation'}
+        {this.state.showNav ? "Close Navigation" : "Open Navigation"}
       </Text>
     );
   }
@@ -41,7 +41,7 @@ export default class Campus extends React.Component {
               <View
                 style={styles.navItem}
                 key={place.id}
-                onEnter={() => this.setState({place: place.image})}
+                onEnter={() => this.setState({ place: place.image })}
               >
                 <Text style={styles.navItemText}>
                   {place.name}
@@ -75,40 +75,40 @@ const styles = {
   navMenu: {
     width: 3,
     height: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    transform: [{translate: [-4, 2, -6]}],
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    transform: [{ translate: [-4, 2, -6] }]
   },
   navButton: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     width: 1,
     height: 0.5,
     borderRadius: 0.2,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 0.01,
-    transform: [{translate: [-4, 2, -5]}],
+    transform: [{ translate: [-4, 2, -5] }]
   },
   navText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 0.15,
-    color: 'rebeccapurple',
+    color: "rebeccapurple"
   },
   navItem: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 1,
     height: 1,
     borderRadius: 0.2,
     borderWidth: 0.02,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   navItemText: {
-    textAlign: 'center',
-    fontSize: '0.2',
-    color: 'steelblue',
-  },
+    textAlign: "center",
+    fontSize: "0.2",
+    color: "steelblue"
+  }
 };
 
-AppRegistry.registerComponent('Campus', () => Campus);
+AppRegistry.registerComponent("Campus", () => Campus);
