eca266d08b0ab8d8d44692e2e10e66575c1733dd
DaniyarJakupov <ddr199305@gmail.com>
2017-06-20 16:12:36 +0200

    Add outside places

--- index.vr.js
+++ index.vr.js
@@ -1,49 +1,58 @@
-
-import React from 'react';
-import Button from 'react'
-import {AppRegistry, asset, Pano, Text, View, Stylesheet, VrButton} from 'react-vr';
+import React from "react";
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  Stylesheet,
+  VrButton
+} from "react-vr";
 
 const places = [
   {
-    name: 'S Bahn Station',
-    image: 'sbahnstation.jpg',
-    id: 1,
+    name: "S Bahn Station",
+    image: "sbahnstation.jpg",
+    id: 1
   },
   {
-    name: 'S Bahn',
-    image: 'sbahn.jpg',
-    id: 2,
+    name: "S Bahn",
+    image: "sbahn.jpg",
+    id: 2
   },
   {
-    name: 'S Bahn Entrance',
-    image: 'sbahn-top.jpg',
-    id: 3,
+    name: "S Bahn Entrance",
+    image: "sbahn-top.jpg",
+    id: 3
   },
   {
-    name: 'Pfaffenwaldring 47e',
-    image: 'pfaffen47.jpg',
-    id: 4,
+    name: "Pfaffenwaldring 47e",
+    image: "pfaffen47.jpg",
+    id: 4
   },
 
   {
-    name: 'QUad',
-    image: 'quad.jpg',
-    id: 5,
+    name: "QUad",
+    image: "quad.jpg",
+    id: 5
   },
 
   {
-    name: 'Library & Cafeteria',
-    image: 'libcafe.jpg',
-    id: 6,
-  },
+    name: "Library & Cafeteria",
+    image: "libcafe.jpg",
+    id: 6
+  }
 ];
 
 export default class Campus extends React.Component {
   constructor() {
     super();
 
-    this.state = {showNav: false, place: 'sbahnstation.jpg', rotation: 130, zoom: -70};
-
+    this.state = {
+      showNav: false,
+      place: "sbahnstation.jpg",
+      location: { lat: "20", lng: "75" }
+    };
   }
 
   toggleNav() {
@@ -58,23 +67,36 @@ export default class Campus extends React.Component {
     );
   }
 
+  geolocation() {
+    return (
+      <Text style={styles.geoText}>
+        Latitude:
+        {this.state.location.lat}
+        , Longitude:
+        {this.state.location.lng}
+      </Text>
+    );
+  }
+
   renderNav() {
     if (this.state.showNav) {
       return (
-        <View style={styles.navMenu}>
-          {places.map(place => {
-            return (
-              <View
-                style={styles.navItem}
-                key={place.id}
-                onEnter={() => this.setState({ place: place.image })}
-              >
-                <Text style={styles.navItemText}>
-                  {place.name}
-                </Text>
-              </View>
-            );
-          })}
+        <View>
+          <View style={styles.navMenu}>
+            {places.map(place => {
+              return (
+                <View
+                  style={styles.navItem}
+                  key={place.id}
+                  onEnter={() => this.setState({ place: place.image })}
+                >
+                  <Text style={styles.navItemText}>
+                    {place.name}
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
         </View>
       );
     }
@@ -92,6 +114,9 @@ export default class Campus extends React.Component {
           {this.renderText()}
         </View>
         {this.renderNav()}
+        <View style={styles.location}>
+          {this.geolocation()}
+        </View>
       </View>
     );
   }
@@ -102,13 +127,13 @@ const styles = {
     width: 3,
     height: 1,
 
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    transform: [{translate: [-5, 3.5, -6]}],
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    transform: [{ translate: [-5, 3.5, -6] }]
   },
   navButton: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     width: 0.5,
 
     height: 0.5,
@@ -119,14 +144,13 @@ const styles = {
     transform: [{ translate: [-4, 2, -5] }]
   },
   navText: {
-
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 0.1,
-    color: 'rebeccapurple',
+    color: "rebeccapurple"
   },
   navItem: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 0.7,
 
     height: 1,
@@ -135,12 +159,19 @@ const styles = {
     backgroundColor: "#fff"
   },
   navItemText: {
-
-    textAlign: 'center',
-    fontSize: '0.11',
-    color: 'steelblue',
+    textAlign: "center",
+    fontSize: 0.11,
+    color: "steelblue"
   },
-
+  location: {
+    width: 7,
+    flexDirection: "row",
+    transform: [{ translate: [-3, 1, -5] }]
+  },
+  geoText: {
+    fontSize: 0.7,
+    color: "red"
+  }
 };
 
 AppRegistry.registerComponent("Campus", () => Campus);
